25f8e95dc135a15b4b13b89ad3bc069af455f345
Shabnam Fatma <shabnamfatma.tech@gmail.com>
2025-12-23 17:49:07 +0530

    added dark and light mode

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const sideMenu = document.querySelector("aside");
 const menuBtn = document.querySelector("#menu-btn");
 const closeBtn = document.querySelector("#close-btn");
+const themeToggler = document.querySelector(".theme-toggler");
 
-menuBtn.addEventListener('click', () => {
-    sideMenu.style.display = 'block';
-})
+menuBtn.addEventListener("click", () => {
+  sideMenu.style.display = "block";
+});
 
-closeBtn.addEventListener('click', () => {
-    sideMenu.style.display = 'none';
-})
+closeBtn.addEventListener("click", () => {
+  sideMenu.style.display = "none";
+});
+
+themeToggler.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme-variables");
+
+  const spans = themeToggler.querySelectorAll("span");
+  const isDark = document.body.classList.contains("dark-theme-variables");
+
+  spans[0].classList.toggle("active", !isDark);
+  spans[1].classList.toggle("active", isDark);
+});
